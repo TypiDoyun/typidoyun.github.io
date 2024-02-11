6effc5bda09c94f30cb8f87d9cb97b57b3daad31
TypiDoyun <121522665+TypiDoyun@users.noreply.github.com>
2024-02-11 20:15:12 +0900

    feat: image slide

---
+++ image-slide/index.ts
@@ -0,0 +1,68 @@
+let index = 0;
+
+const imageSection = document.getElementById("imageSection");
+const moveButtons = Array.from(document.getElementsByClassName("moveButton")) as HTMLElement[];
+
+const setIndex = (value: number) => {
+    index = value;
+
+    if (index < 0 || index >= moveButtons.length) return;
+
+    for (const button of moveButtons) {
+        button.style.backgroundColor = "#00000022";
+    }
+
+    moveButtons[index].style.backgroundColor = "#00000066";
+}
+
+setIndex(index);
+
+const moveImage = (to: number) => {
+    if (isNaN(+to)) return;
+
+    setIndex(to);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+    imageSection.style.left = `calc(var(--image-width) * -${index + 1})`;
+}
+
+const connectImage = (isNext: boolean) => {
+
+    if (isNext) {
+        if (index != 3) return;
+        
+        imageSection.style.transition = "left 0s";
+        moveImage(-1);
+        return true;
+    } else {
+        if (index != 0) return;
+
+        imageSection.style.transition = "left 0s";
+        moveImage(4);
+
+        return true;
+    }
+
+}
+
+const nextImage = () => {
+    connectImage(true)
+    
+    setIndex(++index % 4);
+    
+    setTimeout(() => {
+        imageSection.style.transition = "left .2s ease";
+        moveImage(index);
+    }, 0);
+}
+
+const prevImage = () => {
+    connectImage(false)
+    
+    setIndex(--index % 4);
+    setIndex(index < 0 ? 4 + index : index);
+    
+    setTimeout(() => {
+        imageSection.style.transition = "left .2s ease";
+        moveImage(index);
+    }, 0);
+}
