ea190b61a3b10a5e82b28a164f67965a31ab9630
typidoyun <typidoyun@gmail.com>
2024-03-06 22:46:35 +0900

    feat: cursor

--- calculator/index.js
+++ calculator/index.js
@@ -2,6 +2,7 @@ const calculator = document.querySelector('.calculator');
 const buttons = Array.from(document.querySelectorAll('.button'));
 const formulaElement = document.getElementById('formula');
 const formulaCopyElement = document.getElementById('formula-copy');
+let isVisible = true;
 for (const button of buttons) {
     button.addEventListener("click", clickEvent => {
         switchMode(button);
@@ -31,11 +32,30 @@ const moveHead = (direction) => {
 };
 const addCharacter = (character) => {
     formula = formula.slice(0, head) + character + formula.slice(head);
+    let added = true;
     formula = formula
-        .replace(/([^0-9])0+(\(|-|[0-9]+)/g, "$1$2")
-        .replace(/^0+(\(|-|[0-9]+)/g, "$1")
-        .replace(/(×|÷|-|\+)(×|÷|-|\+)/g, "$2");
-    moveHead("right");
+        .replace(/([^0-9])0+(\(|-|[0-9]+)/g, (_, a, b) => {
+        added = false;
+        return `${a}${b}`;
+    })
+        .replace(/^0+(\(|-|[0-9]+)/g, (_, a) => {
+        added = false;
+        return a;
+    })
+        .replace(/(×|÷|-|\+)(×|÷|-|\+)/g, (_, a, b) => {
+        added = false;
+        return b;
+    })
+        .replace(/^\./g, (_) => {
+        added = false;
+        return "";
+    })
+        .replace(/([^0-9]+)\./g, (_, a) => {
+        added = false;
+        return a;
+    });
+    if (added)
+        moveHead("right");
     updateFormula();
 };
 let fontSize = 22;
@@ -43,25 +63,36 @@ const ratioList = [];
 const updateFormula = () => {
     if (formula === "")
         formula = "0";
-    const temp = `${formula.slice(0, head)}{HeadPoint}${formula.slice(head)}`.replace(/\s*(×|÷|-|\+)\s*/g, " $1 ");
+    const temp = `${formula.slice(0, head)}{HeadPoint}${formula.slice(head)}`
+        .replace(/\s*(×|÷|-|\+)\s*/g, " $1 ")
+        .replaceAll(" ", "&nbsp;");
     const showedFormula = temp.replace("{HeadPoint}", "");
     const formulaCopy = temp.split("{HeadPoint}")[0];
     const headIndex = temp.indexOf("{HeadPoint}");
     if (headIndex === -1)
         return;
     formulaElement.innerHTML = showedFormula;
-    formulaCopyElement.innerHTML = `${formulaCopy.replaceAll(" ", "&nbsp;")}<span class="cursor">|</span>`;
+    formulaCopyElement.innerHTML = `${formulaCopy}`;
+    const cursorElement = document.createElement("span");
+    cursorElement.style.color = `#ffffff${isVisible ? "EE" : "05"}`;
+    cursorElement.textContent = "|";
+    cursorElement.classList.add("cursor");
+    formulaCopyElement.appendChild(cursorElement);
     const ratio = 270 / formulaElement.clientWidth;
     if (ratio > 1) {
         if (ratioList.length === 0)
             return;
         fontSize /= ratioList.pop();
         formulaElement.style.fontSize = `${fontSize.toFixed(2)}px`;
+        cursorElement.style.fontSize = `${fontSize.toFixed(2)}px`;
+        formulaCopyElement.style.fontSize = `${fontSize.toFixed(2)}px`;
         return;
     }
     ratioList.push(ratio);
     fontSize *= ratio;
     formulaElement.style.fontSize = `${fontSize.toFixed(2)}px`;
+    cursorElement.style.fontSize = `${fontSize.toFixed(2)}px`;
+    formulaCopyElement.style.fontSize = `${fontSize.toFixed(2)}px`;
 };
 const getAnswer = () => {
     const result = operateFormula(formula);
@@ -71,12 +102,41 @@ const getAnswer = () => {
     moveHead("none");
     updateFormula();
 };
-let isVisible = true;
 setInterval(() => {
     const cursor = document.getElementsByClassName("cursor")[0];
     if (!cursor)
         return;
-    console.log("on");
-    cursor.style.color = `#ffffff${isVisible ? "00" : "FF"}`;
+    cursor.style.color = `#ffffff${isVisible ? "05" : "EE"}`;
     isVisible = !isVisible;
 }, 500);
+addEventListener("keydown", eventData => {
+    console.log(eventData.key);
+    switch (eventData.key) {
+        case "ArrowLeft":
+            moveHead("left");
+            break;
+        case "ArrowRight":
+            moveHead("right");
+            break;
+        case "Backspace":
+            backspace();
+            break;
+        case ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"].find(key => key === eventData.key):
+            addCharacter(eventData.key);
+            break;
+        case "-":
+            addCharacter("-");
+            break;
+        case "+":
+            addCharacter("+");
+            break;
+        case "*":
+            addCharacter("×");
+            break;
+        case "/":
+            addCharacter("÷");
+            break;
+        case "Enter":
+            getAnswer();
+    }
+});
--- calculator/index.ts
+++ calculator/index.ts
@@ -2,6 +2,7 @@ const calculator = document.querySelector('.calculator') as HTMLDivElement;
 const buttons = Array.from(document.querySelectorAll('.button')) as HTMLSpanElement[];
 const formulaElement = document.getElementById('formula') as HTMLSpanElement;
 const formulaCopyElement = document.getElementById('formula-copy') as HTMLSpanElement;
+let isVisible = true;
 
 for (const button of buttons) {
     button.addEventListener("click", clickEvent => {
@@ -38,11 +39,29 @@ const moveHead = (direction: "right" | "left" | "none") => {
 
 const addCharacter = (character: string) => {
     formula = formula.slice(0, head) + character + formula.slice(head);
+    let added = true;
     formula = formula
-        .replace(/([^0-9])0+(\(|-|[0-9]+)/g, "$1$2")
-        .replace(/^0+(\(|-|[0-9]+)/g, "$1")
-        .replace(/(×|÷|-|\+)(×|÷|-|\+)/g, "$2")
-    moveHead("right");
+        .replace(/([^0-9])0+(\(|-|[0-9]+)/g, (_: string, a: string, b: string) => {
+            added = false;
+            return `${a}${b}`;
+        })
+        .replace(/^0+(\(|-|[0-9]+)/g, (_: string, a: string) => {
+            added = false;
+            return a;
+        })
+        .replace(/(×|÷|-|\+)(×|÷|-|\+)/g, (_: string, a: string, b: string) => {
+            added = false;
+            return b;
+        })
+        .replace(/^\./g, (_: string) => {
+            added = false;
+            return "";
+        })
+        .replace(/([^0-9]+)\./g, (_: string, a: string) => {
+            added = false;
+            return a;
+        });
+    if (added) moveHead("right");
 
     updateFormula();
 }
@@ -53,7 +72,9 @@ const ratioList: number[] = [];
 const updateFormula = () => {
     if (formula === "") formula = "0";
 
-    const temp = `${formula.slice(0, head)}{HeadPoint}${formula.slice(head)}`.replace(/\s*(×|÷|-|\+)\s*/g, " $1 ")
+    const temp = `${formula.slice(0, head)}{HeadPoint}${formula.slice(head)}`
+        .replace(/\s*(×|÷|-|\+)\s*/g, " $1 ")
+        .replaceAll(" ", "&nbsp;");
     const showedFormula = temp.replace("{HeadPoint}", "");
     const formulaCopy = temp.split("{HeadPoint}")[0];
     const headIndex = temp.indexOf("{HeadPoint}");
@@ -61,7 +82,14 @@ const updateFormula = () => {
     if (headIndex === -1) return;
 
     formulaElement.innerHTML = showedFormula;
-    formulaCopyElement.innerHTML = `${formulaCopy.replaceAll(" ", "&nbsp;")}<span class="cursor">|</span>`;
+    formulaCopyElement.innerHTML = `${formulaCopy}`;
+    const cursorElement = document.createElement("span");
+
+    cursorElement.style.color = `#ffffff${isVisible ? "EE" : "05"}`;
+    cursorElement.textContent = "|";
+    cursorElement.classList.add("cursor");
+
+    formulaCopyElement.appendChild(cursorElement);
 
     const ratio = 270 / formulaElement.clientWidth;
     if (ratio > 1) {
@@ -69,12 +97,16 @@ const updateFormula = () => {
 
         fontSize /= ratioList.pop()!;
         formulaElement.style.fontSize = `${fontSize.toFixed(2)}px`;
+        cursorElement.style.fontSize = `${fontSize.toFixed(2)}px`;
+        formulaCopyElement.style.fontSize = `${fontSize.toFixed(2)}px`;
         return;
     }
 
     ratioList.push(ratio);
     fontSize *= ratio;
     formulaElement.style.fontSize = `${fontSize.toFixed(2)}px`;
+    cursorElement.style.fontSize = `${fontSize.toFixed(2)}px`;
+    formulaCopyElement.style.fontSize = `${fontSize.toFixed(2)}px`;
 }
 
 const getAnswer = () => {
@@ -87,15 +119,48 @@ const getAnswer = () => {
     updateFormula();
 }
 
-let isVisible = true;
 
 setInterval(() => {
     const cursor = document.getElementsByClassName("cursor")[0] as HTMLSpanElement;
 
     if (!cursor) return;
 
-    console.log("on");
-
-    cursor.style.color = `#ffffff${isVisible ? "00" : "FF"}`;
+    cursor.style.color = `#ffffff${isVisible ? "05" : "EE"}`;
     isVisible = !isVisible;
 }, 500);
+
+addEventListener("keydown", eventData => {
+    console.log(eventData.key)
+    switch (eventData.key) {
+        case "ArrowLeft":
+            moveHead("left");
+            break;
+        case "ArrowRight":
+            moveHead("right");
+            break;
+
+        case "Backspace":
+            backspace();
+            break;
+
+        case ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"].find(key => key === eventData.key):
+            addCharacter(eventData.key);
+            break;
+        
+        case "-":
+            addCharacter("-");
+            break;
+        case "+":
+            addCharacter("+");
+            break;
+        case "*":
+            addCharacter("×");
+            break;
+        case "/":
+            addCharacter("÷");
+            break;
+
+        case "Enter":
+            getAnswer();
+    }
+})
