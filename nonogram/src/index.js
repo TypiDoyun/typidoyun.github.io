e4aaa86d5a6359738eda938421676a01ba66dd54
typidoyun <typidoyun@gmail.com>
2024-06-26 00:55:45 +0900

    refactor: Prevent context menu from appearing on nonogram board

--- nonogram/src/index.js
+++ nonogram/src/index.js
@@ -1,5 +1,8 @@
 import { board, drawBoard } from "./gui.js";
 const boardElement = document.getElementById("board");
+document.addEventListener("contextmenu", eventData => {
+    eventData.preventDefault();
+});
 window.addEventListener("DOMContentLoaded", () => {
     drawBoard(board);
 });
--- nonogram/src/index.ts
+++ nonogram/src/index.ts
@@ -2,6 +2,10 @@ import { board, drawBoard } from "./gui.js";
 
 const boardElement = document.getElementById("board") as HTMLDivElement;
 
+document.addEventListener("contextmenu", eventData => {
+    eventData.preventDefault();
+})
+
 window.addEventListener("DOMContentLoaded", () => {
     drawBoard(board);
 });
