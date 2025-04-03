3151f3819b86fd4ed5ef567eaba16419d49622aa
typidoyun <typidoyun@gmail.com>
2025-04-03 12:33:08 +0900

    refactor: 'use strict' 추가 및 변수 선언을 const로 변경, TypeScript 설정 수정

--- index.js
+++ index.js
@@ -1,79 +1,79 @@
-var items = Array.from(document.getElementsByClassName("item"));
-var sections = Array.from(document.getElementsByClassName("section"));
-var iframe = document.getElementById("background");
-var item = items[0];
+"use strict";
+const items = Array.from(document.getElementsByClassName("item"));
+const sections = Array.from(document.getElementsByClassName("section"));
+const iframe = document.getElementById("background");
+const item = items[0];
 iframe.width = window.innerWidth.toString();
 iframe.height = (window.innerHeight * 1.2).toString();
 ;
-var mousePosition = {
+const mousePosition = {
     x: 0,
     y: 0
 };
-var getMousePosition = function (eventData) {
+const getMousePosition = (eventData) => {
     mousePosition.x = eventData.clientX;
     mousePosition.y = eventData.clientY;
 };
 addEventListener("mousemove", getMousePosition);
-var move = function (where, behavior) {
-    if (behavior === void 0) { behavior = "smooth"; }
-    var element = document.getElementById("section".concat(where));
+const move = (where, behavior = "smooth") => {
+    const element = document.getElementById(`section${where}`);
     if (!element)
         return;
     window.scrollTo({
         top: item.clientHeight * (where - 1),
-        behavior: behavior
+        behavior
     });
 };
-addEventListener("resize", function () {
+addEventListener("resize", () => {
     move(1, "instant");
 });
-var distanceLimit = 60;
-var _loop_1 = function (index) {
-    var item_1 = items[index];
-    var section = sections[index];
-    var tick = 0;
-    setInterval(function () {
+const distanceLimit = 60;
+for (let index = 0; index < sections.length; index++) {
+    const item = items[index];
+    const section = sections[index];
+    let tick = 0;
+    setInterval(() => {
         if (tick > 0)
             tick--;
     }, 10);
-    var interval;
-    section.addEventListener("mouseenter", function () {
+    let interval;
+    section.addEventListener("mouseenter", () => {
         if (tick > 0)
             return;
         if (window.innerWidth < 1024)
             return;
-        item_1.style.position = "relative";
+        item.style.position = "relative";
         section.style.position = "absolute";
         section.style.transition = "transform 0.5s";
         section.style.left = "0";
         section.style.top = "0";
-        interval = setInterval(function () {
-            var absoluteCenter = {
-                x: item_1.offsetLeft + section.clientWidth / 2,
-                y: item_1.offsetTop + section.clientHeight / 2
+        interval = setInterval(() => {
+            const absoluteCenter = {
+                x: item.offsetLeft + section.clientWidth / 2,
+                y: item.offsetTop + section.clientHeight / 2
             };
-            var center = {
+            const center = {
                 x: absoluteCenter.x + section.offsetLeft,
                 y: absoluteCenter.y + section.offsetTop
             };
-            var direction = {
+            const direction = {
                 x: mousePosition.x - absoluteCenter.x,
                 y: mousePosition.y - absoluteCenter.y
             };
-            var distance = Math.pow((Math.pow(direction.x, 2) + Math.pow(direction.y, 2)), 0.5);
-            var destination = {
+            const distance = (direction.x ** 2 + direction.y ** 2) ** 0.5;
+            const destination = {
                 x: distance > distanceLimit ? direction.x * (distanceLimit / distance) : direction.x,
                 y: distance > distanceLimit ? direction.y * (distanceLimit / distance) : direction.y
             };
-            var destDirection = {
+            const destDirection = {
                 x: destination.x - (center.x - absoluteCenter.x),
                 y: destination.y - (center.y - absoluteCenter.y),
             };
-            section.style.left = "".concat(+section.style.left.replace("px", "") + (destDirection.x / 100), "px");
-            section.style.top = "".concat(+section.style.top.replace("px", "") + (destDirection.y / 50), "px");
+            section.style.left = `${+section.style.left.replace("px", "") + (destDirection.x / 100)}px`;
+            section.style.top = `${+section.style.top.replace("px", "") + (destDirection.y / 50)}px`;
         }, 10);
     });
-    section.addEventListener("mouseleave", function () {
+    section.addEventListener("mouseleave", () => {
         section.style.position = "relative";
         section.style.transition = ".5s";
         section.style.left = "0";
@@ -81,7 +81,4 @@ var _loop_1 = function (index) {
         tick = 1;
         clearInterval(interval);
     });
-};
-for (var index = 0; index < sections.length; index++) {
-    _loop_1(index);
 }
